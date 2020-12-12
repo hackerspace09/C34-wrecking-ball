24aa939150c5d930a31f62cd1843f880d9274dbc
hackerspace09 <46839090+hackerspace09@users.noreply.github.com>
2020-12-11 19:36:47 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# C34-wrecking-ball
-capstone project project 3
+# angryBirdsStage2.5
+Angry Birds stage 2.5 with Class Inheritance and Images
---
+++ Rope.js
@@ -0,0 +1,28 @@
+class Rope{
+    constructor(aBody,bPoint){
+        var options = {
+            bodyA: aBody,
+            pointB: bPoint,
+            stiffness: 1.2,
+            length: 250   
+           }
+            this.rope = Constraint.create(options);
+            World.add(world,this.rope);
+    }
+
+
+    display(){
+        if(this.rope.bodyA){
+            var pointA = this.rope.bodyA.position;
+            var pointB = this.rope.pointB;
+            push();
+            stroke(48,22,8);
+            strokeWeight(3);
+            line(pointA.x,pointA.y,pointB.x,pointB.y)
+
+            pop()
+        }
+       
+    }
+}
+
